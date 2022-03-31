f24d2344d29a6dbc3f34aae4cf1ec3de613ed7f8
chris_chan <chrischandfw@gmail.com>
2022-03-31 18:48:36 -0500

    added event listener to load workshops

--- create-participant/create.js
+++ create-participant/create.js
@@ -1,6 +1,9 @@
-import { createParticipant } from '../fetch-utils';
+import { createParticipant, getWorkshops } from '../fetch-utils';
 
 const form = document.querySelector('form');
+const selectEl = document.querySelector('select');
+
+
 
 form.addEventListener('submit', async e => {
     e.preventDefault();
@@ -8,4 +11,18 @@ form.addEventListener('submit', async e => {
     const data = new FormData(form);
 
     await createParticipant(data.get('name'), data.get('workshop_id'));
+});
+form.reset();
+
+window.addEventListener('load', async () => {
+    const workshops = await getWorkshops();
+
+    for (let workshop of workshops) {
+        const optionEl = document.createElement('option');
+
+        optionEl.textContent = workshop.teams;
+        optionEl.value = workshop.id;
+
+        selectEl.append(optionEl);
+    }
 });
