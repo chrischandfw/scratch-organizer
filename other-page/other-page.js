2d4d089cae56c8e89c9ac5b370ad1089112aae45
chris_chan <chrischandfw@gmail.com>
2022-04-03 15:06:53 -0500

    added async function to display workshops and delete participant

--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,6 +1,7 @@
+/* eslint-disable indent */
 /* eslint-disable no-console */
 // eslint-disable-next-line no-unused-vars
-import { checkAuth, getParticipants, getWorkshops, logout } from '../fetch-utils.js';
+import { checkAuth, deleteParticipant, getParticipants, getWorkshops, logout } from '../fetch-utils.js';
 
 checkAuth();
 
@@ -14,6 +15,12 @@ logoutButton.addEventListener('click', () => {
 window.addEventListener('load', async () => {
     const workshops = await getWorkshops();
 
+    async function displayWorkshops() {
+        const workshops = await getWorkshops();
+
+        workshopsEl.textContent = '';
+    }    
+	
     for (let workshop of workshops) {
         const workshopEl = document.createElement('div');
         const teamEl = document.createElement('h3');
@@ -27,6 +34,12 @@ window.addEventListener('load', async () => {
             const participantEL = document.createElement('p');
 
             participantEL.textContent = participant.name;
+            
+            participantEL.addEventListener('click', async () => {
+				await deleteParticipant(participant.id);
+                displayWorkshops();
+            });
+
             workshopEl.append(participantEL);
             workshopsListEl.append(workshopEl);
         }
